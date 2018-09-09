cfa99e5bb6d7fbb98cf45c87df3df7327b98b71c
Hugo Gomes <hdsg0807@gmail.com>
2018-09-09 19:39:05 -0300

    criamos a classe usuário

--- class/Sql.php
+++ class/Sql.php
@@ -8,14 +8,14 @@ class Sql extends PDO {
 		$this->conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");
 	}
 
-	private function setParams($statment, $parameters = array()){
+	private function setParams($statement, $parameters = array()){
 		foreach ($parameters as $key => $value) {
-			$this->setParam($key, $value);
+			$this->setParam($statement, $key, $value);
 		}
 	}
 
-	private function setParam($statment, $key, $value){
-		$statment->bindParam($key, $value);
+	private function setParam($statement, $key, $value){
+		$statement->bindParam($key, $value);
 	}
 
 	public function query($rawQuery, $params = array()){
@@ -29,8 +29,6 @@ class Sql extends PDO {
 		$stmt = $this->query($rawQuery, $params);
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
-
-
 }
 
 
